2e2a0a4ff58769e1c27799a356f91c2b0b0d9aa6
puleri <puleri.mp4@gmail.com>
2023-06-20 10:39:51 -0700

    INITIAL COMMIT

---
+++ src/main/java/StringCalculator.java
@@ -0,0 +1,22 @@
+public class StringCalculator {
+    public static int add(String numbers) {
+        int returnValue = 0;
+        String[] numbersArray = numbers.split(",|\n");
+        if (numbersArray.length > 2) {
+            throw new RuntimeException("Up to 2 numbers separated by comma (,) are allowed");
+        } else {
+            for (String number : numbersArray) {
+                if (!number.trim().isEmpty()) {
+                    if (Integer.parseInt(number.trim()) >= 1000) {
+                        // Add nothing
+                        returnValue += 0;
+                    } else {
+                        returnValue += Integer.parseInt(number.trim());
+                        // If it is not a number, parseInt will throw an exception
+                    }
+                }
+            }
+        }
+        return returnValue;
+    }
+}
---
+++ src/test/java/StringCalculatorTest.java
@@ -0,0 +1,28 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class StringCalculatorTest {
+
+    private StringCalculator equationString;
+
+    @BeforeEach
+    public void setUp() {
+        equationString = new StringCalculator();
+    }
+
+    @AfterEach
+    public void tearDown() {
+        equationString = null;
+    }
+
+    @Test
+    public void testThousandIgnore() {
+        StringCalculator.add("1, 1000");
+        equals(1);
+    }
+
+}
